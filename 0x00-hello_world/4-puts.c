e58954eaf8ed6e64f36472fbd4c149deb51834cc
Mathew Ajayi <harnessajayi@gmail.com>
2022-09-08 17:18:51 -0700

    updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-/*main.c*/
+/**
+ * main: entry point
+ * Description: print a string using followed by new line
+ * return: 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
