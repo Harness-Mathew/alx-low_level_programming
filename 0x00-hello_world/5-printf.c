7f7a14e454767b692be24d5810b4ab183120869f
Mathew Ajayi <harnessajayi@gmail.com>
2022-09-08 17:34:52 -0700

    l

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+ * main - entry point
+ * Description: Print exactly followed by new line
+ * Return: 0
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+	printf("with proper grammar, but the outcome is a piece of art,");
+		return (0);
 }
