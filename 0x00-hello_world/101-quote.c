bd74f036942262015e962bd9193cdd76dea3a034
Mathew Ajayi <harnessajayi@gmail.com>
2022-09-11 13:13:22 -0700

    p

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * description: stderr print
+ * return: 1
+ **/
+int main()
+{
+	fprintf(stderr, "[and that piece of art is useful" - Dora Korpar, 2015-10-19\n]';
+			return (1);
+}
