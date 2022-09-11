b786c617339a7c21cfc82031fc475f9dca946047
Mathew Ajayi <harnessajayi@gmail.com>
2022-09-11 11:55:51 -0700

    p

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,8 +4,14 @@
  * Description:
  * Return: 0
  **/
-int main()
-{
-	printf();
-	return();
+int main(void)
+{	int intType;
+	char charType;
+	float floatType;
+	double doubleType;
+	printf("Size of int: %zu bytes\n", sizeof(intType));
+	printf("Size of char: %zu bytes\n", sizeof(charType);
+	printf("size of float: %zu bytes\n", sizeof(floatType);
+	printf("size of double: %zu bytes\n", sizeof(doubleType);
+	return(0);
 }
