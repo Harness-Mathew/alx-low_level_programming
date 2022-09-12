74b9da6df9a7abbe6437f30fee11caff2b65aa44
Mathew Ajayi <harnessajayi@gmail.com>
2022-09-12 10:01:27 -0700

    p

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,6 +16,6 @@ int main(void)
 	printf("Size of a long int: %zu byte(s)\n", sizeof(longintType));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongintType));
 	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
-//	fprintf(stderr, "[Anything]");
+	fprintf(stderr, "[Anything]");
 	return (0);
 }
